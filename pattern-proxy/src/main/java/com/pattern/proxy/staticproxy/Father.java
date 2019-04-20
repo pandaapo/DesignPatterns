35f106acdc0d21a5a340a51d9262634b6e5e77f0
pandaapo <1052156701@qq.com>
2019-04-20 17:33:03 +0800

    修改代码：静态代理是显示声明被代理对象，所以应该引入Son，而不是Person

--- pattern-proxy/src/main/java/com/pattern/proxy/staticproxy/Father.java
+++ pattern-proxy/src/main/java/com/pattern/proxy/staticproxy/Father.java
@@ -3,16 +3,16 @@ package com.pattern.proxy.staticproxy;
 import com.pattern.proxy.Person;
 
 public class Father {
-    private Person person;
+    private Son son;
 
     //通过构造方法，把“儿子”的引用传进来
-    public Father(Person person) {
-        this.person = person;
+    public Father(Son person) {
+        this.son = person;
     }
 
     public void findLove(){
         System.out.println("父亲劝结");
-        this.person.findLove();
+        this.son.findLove();
         System.out.println("达成一致，欢欢喜喜");
     }
 }
