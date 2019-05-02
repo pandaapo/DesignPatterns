93cf9122abcf1e2f030277381f02453e77273c79
pandaapo <1052156701@qq.com>
2019-05-02 15:36:04 +0800

    根据网友博客，补充Guava案例的注释

--- pattern-observer/src/main/java/com/pattern/observer/guava/GuavaEvent.java
+++ pattern-observer/src/main/java/com/pattern/observer/guava/GuavaEvent.java
@@ -4,7 +4,7 @@ import com.google.common.eventbus.Subscribe;
 
 public class GuavaEvent {
 
-    //加了该注解，这个方法就会被监听/被观察
+    //加了该注解，这个方法就成为监听器，在"String"事件发成变化时就会触发，传入了String类型参数就表示发生了变化。
     @Subscribe
     public void subscribe(String str){
         System.out.println("执行subscribe方法，传入的参数是:" + str);
--- pattern-observer/src/main/java/com/pattern/observer/guava/GuavaEventTest.java
+++ pattern-observer/src/main/java/com/pattern/observer/guava/GuavaEventTest.java
@@ -2,6 +2,9 @@ package com.pattern.observer.guava;
 
 import com.google.common.eventbus.EventBus;
 
+/**
+ * 一篇介绍Guava的观察者源码和实现的博客：https://www.cnblogs.com/android-blogs/p/5590732.html
+ */
 public class GuavaEventTest {
     //之前两个案例面向的是类，guava面向的是方法。
     public static void main(String[] args) {
